a05258a43ff940794e7e02a2de0f6d4f710c45b8
acies-harini <harini@aciesglobal.com>
2022-11-03 17:40:44 +0530

    add calculator api

--- Sample-Project/src/main/java/com/harini/SampleProject/controller/CalculatorMain.java
+++ Sample-Project/src/main/java/com/harini/SampleProject/controller/CalculatorMain.java
@@ -3,7 +3,7 @@ package com.harini.SampleProject.controller;
 public class CalculatorMain {
     public static void main(String[] args) {
         CalculatorController cc = new CalculatorController();
-        Double result = cc.add(5.7,5.7);
+        Double result = cc.add(5.7,5.7,6.0);
         System.out.println(result);
     }
 }
--- Sample-Project/src/main/java/com/harini/SampleProject/dto/CalculatorDTO.java
+++ Sample-Project/src/main/java/com/harini/SampleProject/dto/CalculatorDTO.java
@@ -1,4 +1,64 @@
 package com.harini.SampleProject.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class CalculatorDTO {
+
+    private Double num1;
+    private Double num2;
+    private Double num3;
+    private Double num4;
+    private Double num5;
+    @JsonProperty("num61")
+    private Double num6;
+
+    public Double getNum1() {
+        return num1;
+    }
+
+    public void setNum1(Double num1) {
+        this.num1 = num1;
+    }
+
+    public Double getNum2() {
+        return num2;
+    }
+
+    public void setNum2(Double num2) {
+        this.num2 = num2;
+    }
+
+    public Double getNum3() {
+        return num3;
+    }
+
+    public void setNum3(Double num3) {
+        this.num3 = num3;
+    }
+
+    public Double getNum4() {
+        return num4;
+    }
+
+    public void setNum4(Double num4) {
+        this.num4 = num4;
+    }
+
+    public Double getNum5() {
+        return num5;
+    }
+
+    public void setNum5(Double num5) {
+        this.num5 = num5;
+    }
+
+    public Double getNum6() {
+        return num6;
+    }
+
+    public void setNum6(Double num6) {
+        this.num6 = num6;
+    }
+
+
 }
